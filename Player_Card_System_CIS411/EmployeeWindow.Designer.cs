d3d72d9166fea85b5b77f783b7df0b2fc17e8017
MikeFredley <36174846+MikeFredley@users.noreply.github.com>
2021-02-17 18:31:10 -0500

    Added example chart photo and removed the tab menu in the EmployeeWindow

--- Player_Card_System_CIS411/EmployeeWindow.Designer.cs
+++ Player_Card_System_CIS411/EmployeeWindow.Designer.cs
@@ -29,26 +29,8 @@ namespace Player_Card_System_CIS411
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmployeeWindow));
             this.btnLogout = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.roundsPage = new System.Windows.Forms.TabPage();
-            this.label7 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.btnSubmit = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.addPage = new System.Windows.Forms.TabPage();
-            this.btnDeleteUser = new System.Windows.Forms.Button();
-            this.btnAddUser = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.adminPage = new System.Windows.Forms.TabPage();
-            this.btnImportData = new System.Windows.Forms.Button();
-            this.btnReset = new System.Windows.Forms.Button();
-            this.btnGetData = new System.Windows.Forms.Button();
             this.btnSearch = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -57,14 +39,9 @@ namespace Player_Card_System_CIS411
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.btnAddAccount = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.tabControl1.SuspendLayout();
-            this.roundsPage.SuspendLayout();
-            this.addPage.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.adminPage.SuspendLayout();
+            this.pcbExampleGraph = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbExampleGraph)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLogout
@@ -76,199 +53,6 @@ namespace Player_Card_System_CIS411
             this.btnLogout.Text = "Log Out";
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(20, 71);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(84, 17);
-            this.radioButton1.TabIndex = 10;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Add Rounds";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(110, 71);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(100, 17);
-            this.radioButton2.TabIndex = 11;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Deduct Rounds";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 112);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(99, 13);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "Number of Rounds:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(138, 109);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 14;
-            // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.roundsPage);
-            this.tabControl1.Controls.Add(this.addPage);
-            this.tabControl1.Controls.Add(this.adminPage);
-            this.tabControl1.Location = new System.Drawing.Point(22, 406);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(782, 248);
-            this.tabControl1.TabIndex = 17;
-            // 
-            // roundsPage
-            // 
-            this.roundsPage.Controls.Add(this.label7);
-            this.roundsPage.Controls.Add(this.comboBox1);
-            this.roundsPage.Controls.Add(this.btnSubmit);
-            this.roundsPage.Controls.Add(this.label6);
-            this.roundsPage.Controls.Add(this.label4);
-            this.roundsPage.Controls.Add(this.radioButton2);
-            this.roundsPage.Controls.Add(this.label5);
-            this.roundsPage.Controls.Add(this.textBox2);
-            this.roundsPage.Controls.Add(this.radioButton1);
-            this.roundsPage.Location = new System.Drawing.Point(4, 22);
-            this.roundsPage.Name = "roundsPage";
-            this.roundsPage.Padding = new System.Windows.Forms.Padding(3);
-            this.roundsPage.Size = new System.Drawing.Size(774, 222);
-            this.roundsPage.TabIndex = 0;
-            this.roundsPage.Text = "Rounds";
-            this.roundsPage.UseVisualStyleBackColor = true;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(17, 152);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(115, 13);
-            this.label7.TabIndex = 19;
-            this.label7.Text = "Transaction Employee:";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(138, 149);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 18;
-            // 
-            // btnSubmit
-            // 
-            this.btnSubmit.Location = new System.Drawing.Point(20, 184);
-            this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(84, 23);
-            this.btnSubmit.TabIndex = 17;
-            this.btnSubmit.Text = "Submit";
-            this.btnSubmit.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(156, 41);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(13, 13);
-            this.label6.TabIndex = 16;
-            this.label6.Text = "0";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 41);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(133, 13);
-            this.label4.TabIndex = 15;
-            this.label4.Text = "Current Rounds Available: ";
-            // 
-            // addPage
-            // 
-            this.addPage.Controls.Add(this.btnDeleteUser);
-            this.addPage.Controls.Add(this.btnAddUser);
-            this.addPage.Controls.Add(this.dataGridView1);
-            this.addPage.Location = new System.Drawing.Point(4, 22);
-            this.addPage.Name = "addPage";
-            this.addPage.Padding = new System.Windows.Forms.Padding(3);
-            this.addPage.Size = new System.Drawing.Size(774, 222);
-            this.addPage.TabIndex = 1;
-            this.addPage.Text = "Add/Delete Account";
-            this.addPage.UseVisualStyleBackColor = true;
-            // 
-            // btnDeleteUser
-            // 
-            this.btnDeleteUser.Location = new System.Drawing.Point(158, 189);
-            this.btnDeleteUser.Name = "btnDeleteUser";
-            this.btnDeleteUser.Size = new System.Drawing.Size(102, 23);
-            this.btnDeleteUser.TabIndex = 2;
-            this.btnDeleteUser.Text = "Delete User";
-            this.btnDeleteUser.UseVisualStyleBackColor = true;
-            // 
-            // btnAddUser
-            // 
-            this.btnAddUser.Location = new System.Drawing.Point(31, 189);
-            this.btnAddUser.Name = "btnAddUser";
-            this.btnAddUser.Size = new System.Drawing.Size(102, 23);
-            this.btnAddUser.TabIndex = 1;
-            this.btnAddUser.Text = "Create New User";
-            this.btnAddUser.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 26);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(730, 141);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // adminPage
-            // 
-            this.adminPage.Controls.Add(this.button3);
-            this.adminPage.Controls.Add(this.button2);
-            this.adminPage.Controls.Add(this.btnImportData);
-            this.adminPage.Controls.Add(this.btnReset);
-            this.adminPage.Controls.Add(this.btnGetData);
-            this.adminPage.Location = new System.Drawing.Point(4, 22);
-            this.adminPage.Name = "adminPage";
-            this.adminPage.Size = new System.Drawing.Size(774, 222);
-            this.adminPage.TabIndex = 2;
-            this.adminPage.Text = "Admin";
-            this.adminPage.UseVisualStyleBackColor = true;
-            // 
-            // btnImportData
-            // 
-            this.btnImportData.Location = new System.Drawing.Point(23, 37);
-            this.btnImportData.Name = "btnImportData";
-            this.btnImportData.Size = new System.Drawing.Size(99, 49);
-            this.btnImportData.TabIndex = 2;
-            this.btnImportData.Text = "Backup";
-            this.btnImportData.UseVisualStyleBackColor = true;
-            // 
-            // btnReset
-            // 
-            this.btnReset.Location = new System.Drawing.Point(258, 37);
-            this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(99, 49);
-            this.btnReset.TabIndex = 1;
-            this.btnReset.Text = "Reset Season";
-            this.btnReset.UseVisualStyleBackColor = true;
-            // 
-            // btnGetData
-            // 
-            this.btnGetData.Location = new System.Drawing.Point(141, 37);
-            this.btnGetData.Name = "btnGetData";
-            this.btnGetData.Size = new System.Drawing.Size(99, 49);
-            this.btnGetData.TabIndex = 0;
-            this.btnGetData.Text = "Export Accounts";
-            this.btnGetData.UseVisualStyleBackColor = true;
             // 
             // btnSearch
             // 
@@ -339,29 +123,21 @@ namespace Player_Card_System_CIS411
             this.button1.Text = "Admin";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // pcbExampleGraph
             // 
-            this.button2.Location = new System.Drawing.Point(23, 106);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 49);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Restore";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(141, 106);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 49);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Export Transactions";
-            this.button3.UseVisualStyleBackColor = true;
+            this.pcbExampleGraph.Image = ((System.Drawing.Image)(resources.GetObject("pcbExampleGraph.Image")));
+            this.pcbExampleGraph.Location = new System.Drawing.Point(9, 79);
+            this.pcbExampleGraph.Name = "pcbExampleGraph";
+            this.pcbExampleGraph.Size = new System.Drawing.Size(563, 227);
+            this.pcbExampleGraph.TabIndex = 26;
+            this.pcbExampleGraph.TabStop = false;
             // 
             // EmployeeWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(886, 666);
+            this.ClientSize = new System.Drawing.Size(788, 408);
+            this.Controls.Add(this.pcbExampleGraph);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAddAccount);
             this.Controls.Add(this.dataGridView2);
@@ -371,18 +147,12 @@ namespace Player_Card_System_CIS411
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnLogout);
-            this.Controls.Add(this.tabControl1);
             this.Name = "EmployeeWindow";
             this.Text = "Employee";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EmployeeWindow_FormClosing);
             this.Load += new System.EventHandler(this.Employee_Load);
-            this.tabControl1.ResumeLayout(false);
-            this.roundsPage.ResumeLayout(false);
-            this.roundsPage.PerformLayout();
-            this.addPage.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.adminPage.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbExampleGraph)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -390,34 +160,14 @@ namespace Player_Card_System_CIS411
 
         #endregion
         private System.Windows.Forms.Button btnLogout;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage roundsPage;
-        private System.Windows.Forms.Button btnSubmit;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TabPage addPage;
-        private System.Windows.Forms.TabPage adminPage;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnGetData;
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.Button btnReset;
-        private System.Windows.Forms.Button btnDeleteUser;
-        private System.Windows.Forms.Button btnAddUser;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button btnImportData;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button btnAddAccount;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pcbExampleGraph;
     }
 }
