8210779ad5189689050a83df1ec8f00503a51285
MikeFredley <36174846+MikeFredley@users.noreply.github.com>
2021-03-01 18:40:49 -0500

    Up to date

--- Player_Card_System_CIS411/Properties/Settings.Designer.cs
+++ Player_Card_System_CIS411/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Player_Card_System_CIS411.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace Player_Card_System_CIS411.Properties {
         public string OceanVillagePlayerCardConnectionString {
             get {
                 return ((string)(this["OceanVillagePlayerCardConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MICHAEL-PC\\SERVER2019;Initial Catalog=OceanVillagePlayerCard;Integrat" +
+            "ed Security=True")]
+        public string OceanVillagePlayerCardConnectionString1 {
+            get {
+                return ((string)(this["OceanVillagePlayerCardConnectionString1"]));
             }
         }
     }
