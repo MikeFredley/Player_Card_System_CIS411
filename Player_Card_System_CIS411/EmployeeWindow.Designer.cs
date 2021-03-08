c44e74771f051be0dc7635065c4d8c2791154e46
MikeFredley <36174846+MikeFredley@users.noreply.github.com>
2021-03-08 17:33:03 -0500

    Fix

--- Player_Card_System_CIS411/EmployeeWindow.Designer.cs
+++ Player_Card_System_CIS411/EmployeeWindow.Designer.cs
@@ -60,7 +60,7 @@ namespace Player_Card_System_CIS411
             this.btnSearch.TabIndex = 22;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+           // this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // label3
             // 
@@ -77,7 +77,7 @@ namespace Player_Card_System_CIS411
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(200, 20);
             this.txtSearch.TabIndex = 21;
-            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+           // this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
             // label2
             // 
