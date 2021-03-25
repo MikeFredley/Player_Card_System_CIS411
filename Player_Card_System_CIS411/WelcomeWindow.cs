d2f2ce862e589fb6bec1534c6971d2307ec75fe4
MikeFredley <36174846+MikeFredley@users.noreply.github.com>
2021-03-25 19:46:17 -0400

    Made code add default user account for first time setup

--- Player_Card_System_CIS411/WelcomeWindow.cs
+++ Player_Card_System_CIS411/WelcomeWindow.cs
@@ -15,7 +15,28 @@ namespace Player_Card_System_CIS411
         EmployeeWindow employeeScreen;
         public WelcomeWindow()
         {
+            bool exists = false;
             InitializeComponent();
+            foreach (EmployeeInfo emp in Database.EmployeeInfo)
+            {
+                if (emp.FirstName == "Default")
+                {
+                    exists = true;
+                    break;
+                }
+            }    
+
+            if (!exists)
+            {
+                EmployeeInfo employee = new EmployeeInfo();
+                employee.FirstName = "Default";
+                employee.LastName = "Default";
+                employee.UserName = "Default";
+                employee.Password = "Default";
+                employee.IsAdmin = true;
+                employee.IsCurrent = true;
+                Database.AddNewEmployee(employee);
+            }
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -29,12 +50,7 @@ namespace Player_Card_System_CIS411
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
-        {
-         /*   employeeScreen = new EmployeeWindow(this);
-            employeeScreen.Visible = true;
-            this.Hide(); */
-           
-
+        {         
             bool login = Database.Login(txtUsername.Text, txtPassword.Text);
             if (login)
             {
